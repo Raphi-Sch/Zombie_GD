002aa457e6a396c7a24abb8c458a2f37d57e933a
raphi-sch <raphael.scharff@gmail.com>
2022-03-28 11:03:51 +0200

    Add player 1 logic

--- script/Player.gd
+++ script/Player.gd
@@ -1,16 +1,41 @@
 extends Area2D
 
+export var speed = 200
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var screen_size
+var player = 1
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	# The instance is player 1
+	if player == 1:
+		if Input.is_action_pressed("p1_right"):
+			velocity.x += 1
+		if Input.is_action_pressed("p1_left"):
+			velocity.x -= 1
+		if Input.is_action_pressed("p1_down"):
+			velocity.y += 1
+		if Input.is_action_pressed("p1_up"):
+			velocity.y -= 1
+	
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	$AnimatedSprite.flip_v = false
+	if velocity.x != 0:
+		$AnimatedSprite.flip_h = velocity.x < 0
