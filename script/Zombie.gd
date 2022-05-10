06c7693f1cc81b811931b565ab00552999fdd48e
Raphi_55 <35998957+Raphi-Sch@users.noreply.github.com>
2022-05-10 15:49:58 +0200

    Adding zombies

---
+++ script/Zombie.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+export (int) var pos_x
+export (int) var pos_y
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
